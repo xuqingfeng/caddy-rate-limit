2552fa17ee105936e18f93d111cbb5544f4fb095
xuqingfeng <js-xqf@hotmail.com>
2016-11-29 10:57:05 +0800

    Fix setup_test & rm go 1.6

--- setup_test.go
+++ setup_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestSetup(t *testing.T) {
 
-	c := caddy.NewTestController("http", `ratelimit / 2 2`)
+	c := caddy.NewTestController("http", `ratelimit / 2 2 second`)
 	err := setup(c)
 	if err != nil {
 		t.Errorf("Expected no errors, got: %v", err)
@@ -29,27 +29,27 @@ func TestRateLimitParse(t *testing.T) {
 		expected  []Rule
 	}{
 		{
-			`ratelimit / 2 0`, false, []Rule{
-				{2, 0, []string{"/"}},
+			`ratelimit / 2 0 second`, false, []Rule{
+				{2, 0, []string{"/"}, "second"},
 			},
 		},
 		{
-			`ratelimit / 2 1`, false, []Rule{
-				{2, 1, []string{"/"}},
+			`ratelimit / 2 1 second`, false, []Rule{
+				{2, 1, []string{"/"}, "second"},
 			},
 		},
 		{
-			`ratelimit / notFloat64 0`, true, []Rule{},
+			`ratelimit / notFloat64 0 second`, true, []Rule{},
 		},
 		{
-			`ratelimit / 2 0.1`, true, []Rule{},
+			`ratelimit / 2 0.1 second`, true, []Rule{},
 		},
 		{
-			`ratelimit 2 2 {
+			`ratelimit 2 2 second {
                 /resource0
                 /resource1
             }`, false, []Rule{
-				{2, 2, []string{"/resource0", "/resource1"}},
+				{2, 2, []string{"/resource0", "/resource1"}, "second"},
 			},
 		},
 	}
