f629b8d74d31a32fde61ba1da8aa10548a51ff41
xuqingfeng <js-xqf@hotmail.com>
2016-12-14 11:46:16 +0800

    Fix

--- caddylimiter_test.go
+++ caddylimiter_test.go
@@ -22,13 +22,13 @@ func TestAllowAndRetryAfter(t *testing.T) {
 			[]string{"127.0.0.1", "/"}, Rule{Rate: 2, Burst: 2, Unit: "second"}, 2, 0, false, true,
 		},
 		{
-			[]string{"127.0.0.1", "/"}, Rule{Rate: 1, Burst: 2, Unit: "minute"}, 2, 1 * time.Minute, false, true,
+			[]string{"127.0.0.1", "/"}, Rule{Rate: 1, Burst: 2, Unit: "minute"}, 2, 30 * time.Second, false, true,
 		},
 		{
 			[]string{"127.0.0.1", "/"}, Rule{Rate: 1, Burst: 0, Unit: "hour"}, 1, rate.InfDuration, false, false,
 		},
 		{
-			[]string{"127.0.0.1", "/"}, Rule{Rate: 0, Burst: 0}, 2, rate.InfDuration, false, true,
+			[]string{"127.0.0.1", "/"}, Rule{Rate: 0, Burst: 0}, 2, 0, false, true,
 		},
 	}
 
